449056559d190a1be5ae97d707e7bfa005648731
dharmikumbhani <dharmikumbhani201@gmail.com>
2023-03-26 18:40:15 +0530

    Replaced a with Link

--- site/components/Navbar.js
+++ site/components/Navbar.js
@@ -2,13 +2,14 @@ import Image from 'next/image'
 import { useEffect } from 'react'
 import {ConnectButton} from '@rainbow-me/rainbowkit'
 import {useAccount} from 'wagmi'
+import Link from 'next/link'
 
 export default function NavBar(props) {
   return (
     <>
     <nav className='flex-row '>
         <div className="logo-container">
-            <a href='/#'>
+            <Link href='/#'>
             <Image
               src="/feedhook.svg"
               alt="Vercel Logo"
@@ -16,7 +17,7 @@ export default function NavBar(props) {
               height={100}
               priority
             />
-            </a>
+            </Link>
         </div>
         <div className="user-loggedIn">
           { props.isConnected ? (
